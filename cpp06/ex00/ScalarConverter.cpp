4e6b9ab7891e6986a605c8fcd7ba26eabcfdfcc3
Axel Baudrillart <axbaudri@f6r4s10.clusters.42paris.fr>
2025-09-12 17:11:02 +0200

    updated cpp06/ex00

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,1 +1,35 @@
 #include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+    std::cout << "ScalarConverter : default constructor called" << std::endl;
+}
+
+ScalarConverter::~ScalarConverter()
+{
+    std::cout << "ScalarConverter : destructor called" << std::endl;
+}
+
+char ScalarConverter::convertToChar(char *content)
+{
+    char c;
+
+    if (is_digit(content))
+    {
+        int nb = atoi(content);
+        c = static_cast<char>(nb);
+    }
+    else
+        c = static_cast<char>(*content);
+    return c;
+}
+
+int ScalarConverter::convertToInt(char *content)
+{
+    int c;
+    if (is_digit(content))
+        c = atoi(content);
+    else
+        c = static_cast<int>(*content);
+    return c;
+}
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,9 +1,19 @@
 #ifndef SCALAR_CONVERTER_HPP
 #define SCALAR_CONVERTER_HPP
+#include <iostream>
+#include <stdlib.h>
+
+int is_digit(const char *arg);
 
 class ScalarConverter {
 
     public:
+
+    ScalarConverter();
+    ~ScalarConverter();
+    char convertToChar(char *content);
+    int convertToInt(char *content);
+
 
     private:
 };
